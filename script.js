aaf2a6c511ca9a6d6881a234f3649eb7e48281be
zhuravlev-blog-kraken <v.zyravlev@yandex.ru>
2024-01-03 16:36:07 +0300

    clear comment

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-// function checkText() {
-//     let inputs = document.querySelectorAll('input');
-//     let textToCheck = inputs[0].value;
-    
-//     for (let i = 1; i < inputs.length; i++) {
-//         if (inputs[i].value !== textToCheck) {
-//             alert('Текст в полях ввода не совпадает.');
-//             return;
-//         }
-//     }
-//     alert('Текст в полях ввода совпадает.');
-// }
-
-// window.onload = function() {
-//     let inputsDiv = document.getElementById('inputs');
-//     for (let i = 0; i < 5; i++) {
-//         let input = document.createElement('input');
-//         inputsDiv.appendChild(input);
-//     }
-// }
-
-
-//№2
-// let inputCount = 1; // Счетчик для отслеживания количества полей
-
-// function createInput() {
-//     let input = document.createElement('input');
-//     input.setAttribute('placeholder', 'Введите текст');
-//     input.setAttribute('id', 'input' + inputCount);
-//     document.getElementById('inputs').appendChild(input);
-//     inputCount++;
-// }
-
-// function checkText() {
-//     let inputs = document.querySelectorAll('input');
-//     let textToCheck = inputs[0].value;
-
-//     for (let i = 1; i < inputs.length; i++) {
-//         if (inputs[i].value !== textToCheck) {
-//             alert('Текст в полях ввода не совпадает.');
-//             return;
-//         }
-//     }
-//     alert('Текст в полях ввода совпадает.');
-//     createInput();
-// }
-
-// №3
 window.onload = function() {
     createInput();
 }
@@ -65,20 +17,6 @@ function createInput() {
         alert('Достигнуто максимальное количество полей.');
     }
 }
-
-// function checkText() {
-//     let inputs = document.querySelectorAll('input');
-//     let textToCheck = inputs[inputs.length - 1].value; //Проверяем только последнее введенное значение
-//     alert(inputs[inputs.length - 1].value);
-//     for (let i = 0; i < inputs.length - 1; i++) {
-//         if (inputs[i].value !== textToCheck) {
-//             alert('Текст в полях ввода не совпадает. = ');
-//             return;
-//         }
-//     }
-//     alert('Текст в полях ввода совпадает.'+inputCount);
-//     createInput();
-// }
 
 function checkText() {
     let inputs = document.querySelectorAll('input');
